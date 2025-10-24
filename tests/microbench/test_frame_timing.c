ad1d40bbe4e64a8aef74aa93e70beed1bc5c09a5
d954mas <d954mas@gmail.com>
2025-10-24 23:42:20 +0500

    fixed build

--- tests/microbench/test_frame_timing.c
+++ tests/microbench/test_frame_timing.c
@@ -2,24 +2,6 @@
 
 #include "engine/core/nt_engine.h"
 
-#ifdef _WIN32
-#include <windows.h>
-static double now_ms(void) {
-    LARGE_INTEGER freq;
-    LARGE_INTEGER counter;
-    QueryPerformanceFrequency(&freq);
-    QueryPerformanceCounter(&counter);
-    return (double)counter.QuadPart * 1000.0 / (double)freq.QuadPart;
-}
-#else
-#include <time.h>
-static double now_ms(void) {
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    return (double)ts.tv_sec * 1000.0 + (double)ts.tv_nsec / 1e6;
-}
-#endif
-
 int main(void) {
     if (nt_engine_init() != NT_RESULT_OK) {
         fprintf(stderr, "engine init failed\n");
@@ -27,11 +9,11 @@ int main(void) {
     }
 
     const int frames = 1000;
-    double start = now_ms();
+    double start = 0;
     for (int i = 0; i < frames; ++i) {
         //nt_engine_frame();
     }
-    double elapsed = now_ms() - start;
+    double elapsed = 0 - start;
 
     nt_engine_shutdown();
 
